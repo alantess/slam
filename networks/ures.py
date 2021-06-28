6cc9ff82b15bd2ed0e02b73d0ca72a840533e387
Alan <alantessier97@gmail.com>
2021-06-27 22:11:01 -0400

    new resnet model

--- networks/ures.py
+++ networks/ures.py
@@ -1,53 +1,44 @@
 import os
 import torch
-from torch import nn, optim
+from torch import nn
 from torchvision import models
 
 
-class DisparityNet(nn.Module):
-    """
-    Resnet50 U-Net Module
-    """
+class URes152(nn.Module):
     def __init__(self,
-                 n_out_channels=3,
-                 chkpt_dir="model_checkpoints",
-                 model_name="disparity_net.pt"):
-        super(DisparityNet, self).__init__()
-        self.chkpt_dir = chkpt_dir
-        self.file = os.path.join(chkpt_dir, model_name)
-        resnet = models.resnet50(True)
+                 n_channels=1,
+                 model_name='ures152.pt',
+                 chkpt='model_checkpoints'):
+        super(URes152, self).__init__()
+        self.chkpt_dir = chkpt
+        self.file = os.path.join(chkpt, model_name)
+        self.activation = nn.SELU()
+        # Resnet
+        resnet = models.resnet152(True)
         modules = list(resnet.children())
-        # Original Image
-        self.original_conv1 = nn.Conv2d(3, 64, 1, 1)
-        self.original_conv2 = nn.Conv2d(64, 64, 1, 1)
-        # RESNET 50 Encoder
         self.layer1 = nn.Sequential(*modules[:3])
         self.layer2 = nn.Sequential(*modules[3:5])
         self.layer3 = nn.Sequential(*modules[5:6])
         self.layer4 = nn.Sequential(*modules[6:7])
         self.layer5 = nn.Sequential(*modules[7:8])
-        # Upsampling
-        self.upsample = nn.Upsample(scale_factor=2,
-                                    mode='bilinear',
-                                    align_corners=True)
+
+        self.upsample = nn.Upsample(scale_factor=2, mode='nearest')
         self.layer5_up = nn.Conv2d(2048, 2048, 1)
         self.layer4_up = nn.Conv2d(1024, 1024, 1)
         self.layer3_up = nn.Conv2d(512, 512, 1)
         self.layer2_up = nn.Conv2d(256, 256, 1)
         self.layer1_up = nn.Conv2d(64, 64, 1)
 
-        # Reduce the channels
         self.conv5_up = nn.Conv2d(2048 + 1024, 1024, 1, 1)
         self.conv4_up = nn.Conv2d(1024 + 512, 512, 1, 1)
         self.conv3_up = nn.Conv2d(512 + 256, 256, 1, 1)
         self.conv2_up = nn.Conv2d(256 + 64, 64, 1, 1)
-        self.conv1_up = nn.Conv2d(64 + 64, n_out_channels, 1, 1)
+        self.conv1_up = nn.Conv2d(64 + 64, n_channels, 1, 1)
 
-        self.activation = nn.SELU()
+        self.original_conv1 = nn.Conv2d(3, 64, 1, 1)
 
     def forward(self, x):
-        original = self.activation(self.original_conv1(x))
-        original = self.activation(self.original_conv2(original))
+        original = self.original_conv1(x)
         # Encoder
         layer1 = self.layer1(x)
         layer2 = self.layer2(layer1)
@@ -79,7 +70,6 @@ class DisparityNet(nn.Module):
         x = self.upsample(x)
         x = torch.cat([x, original], dim=1)
         x = self.conv1_up(x)
-
         return x
 
     def save(self):
@@ -89,3 +79,9 @@ class DisparityNet(nn.Module):
 
     def load(self):
         self.load_state_dict(torch.load(self.file))
+
+
+# if __name__ == '__main__':
+#     example = torch.zeros(1, 3, 512, 512)
+#     model = URes152()
+#     z = model(example)
